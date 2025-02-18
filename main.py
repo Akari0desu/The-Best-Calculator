b46d99109fb939387016256135cb27e258d449c2
Jasper DAI <dddd_d917@163.com>
2025-02-18 15:30:41 +0000

    Final Version
    Rename“Calculator” to "calculator"
    
    Uploaded main() function to call GUI module

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 14:28:38 2025
+
+@author: jaspersdocument
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Main entry point for the calculator application.
+"""
+from gui import run_gui  # 从 GUI 模块导入运行函数
+
+if __name__ == "__main__":
+    run_gui()  # 启动 GUI
